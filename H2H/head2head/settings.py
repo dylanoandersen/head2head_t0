402aadc853cbf66811e9f048a19ded14a03be2b3
Brad Kinney <bmkinney6@gmail.com>
2025-01-27 13:47:27 -0600

    Setup for brad computer

--- H2H/head2head/settings.py
+++ H2H/head2head/settings.py
@@ -82,9 +82,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'h2h',  # Replace with your database name
-        'USER': 'dandersen',      # Replace with your MySQL username
-        'PASSWORD': 'dylan3',  # Replace with your MySQL password
-        'HOST': '172.17.53.5',      # Replace with your IP address (e.g., 192.168.1.100)
+        'USER': 'bmkinney',      # Replace with your MySQL username
+        'PASSWORD': '0Cyclone6!',  # Replace with your MySQL password
+        'HOST': 'Brads-MacBook-Pro.local',      # Replace with your IP address (e.g., 192.168.1.100)
         'PORT': '3306',                # MySQL default port
     }
 }
