025c48ab03855f30a87c681c5154a1e0d9c239fd
bmkinney6 <bmkinney6@gmail.com>
2025-02-06 22:03:25 -0600

    brad test

--- H2H/head2head/settings.py
+++ H2H/head2head/settings.py
@@ -63,7 +63,6 @@ INSTALLED_APPS = [
     'User',
     'corsheaders',
     'rest_framework_simplejwt',
-    'User',
 
 ]
 
@@ -110,9 +109,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'h2h',  # Replace with your database name
-        'USER': 'dandersen',      # Replace with your MySQL username
-        'PASSWORD': 'dylan3',  # Replace with your MySQL password
-        'HOST': '172.17.39.79',      # Replace with your IP address (e.g., 192.168.1.100)
+        'USER': 'bmkinney',      # Replace with your MySQL username
+        'PASSWORD': '0Cyclone6!',  # Replace with your MySQL password
+        'HOST': 'Brads-MacBook-Pro.local',      # Replace with your IP address (e.g., 192.168.1.100)
         'PORT': '3306',                # MySQL default port
     }
 } 
