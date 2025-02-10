7e53c723a019cb7a292e2b08c8b6be4e7beb38bc
bmkinney6 <bmkinney6@gmail.com>
2025-02-09 20:36:09 -0600

    Brad test Branch

--- H2H/head2head/settings.py
+++ H2H/head2head/settings.py
@@ -109,10 +109,10 @@ WSGI_APPLICATION = 'head2head.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'h2h',  # Replace with your database name
-        'USER': 'dandersen',      # Replace with your MySQL username
-        'PASSWORD': 'dylan3',  # Replace with your MySQL password
-        'HOST': '172.17.53.31',      # Replace with your IP address (e.g., 192.168.1.100)
+        'NAME': 'H2H',  # Replace with your database name
+        'USER': 'bmkinney',      # Replace with your MySQL username
+        'PASSWORD': '0Cyclone6!',  # Replace with your MySQL password
+        'HOST': 'Brads-MacBook-Pro.local',      # Replace with your IP address (e.g., 192.168.1.100)
         'PORT': '3306',                # MySQL default port
     }
 } 
